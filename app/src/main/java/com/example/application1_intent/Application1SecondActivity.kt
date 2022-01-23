c638f76f2f0252c0a046200b73e113b6b6b6fbdb
MelHerrera <72715151+MelHerrera@users.noreply.github.com>
2022-01-23 15:37:30 -0600

    Initial commit

---
+++ app/src/main/java/com/example/application1_intent/Application1SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.application1_intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Application1SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application1_second)
+    }
+}
